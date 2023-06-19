1b6cfe538754fabf67da478a11c7c65d013b7bca
Kornidesz Máté <dev.profile.km@gmail.com>
2023-06-19 22:43:08 +0200

    Views started to elaborate for controller purposes

--- NCloud/NCloud/Controllers/HomeController.cs
+++ NCloud/NCloud/Controllers/HomeController.cs
@@ -6,19 +6,9 @@ namespace NCloud.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(){ }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
