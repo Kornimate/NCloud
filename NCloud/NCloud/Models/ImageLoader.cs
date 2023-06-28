c9befcf468a11a12642ca3a3e48bc7a841fc28a1
Kornidesz Máté <dev.profile.km@gmail.com>
2023-06-28 17:27:54 +0200

    Logo added to project

--- NCloud/NCloud/Models/ImageLoader.cs
+++ NCloud/NCloud/Models/ImageLoader.cs
@@ -18,7 +18,7 @@ namespace NCloud.Models
 
         private static Dictionary<string, string> iconPaths = new Dictionary<string, string>()
         {
-            {"Folder",@"/utilities/folder.svg" },
+            {"Folder",@"/utilities/folder2.svg" },
             {"File",@"/utilities/file.svg" }
         };
     }
