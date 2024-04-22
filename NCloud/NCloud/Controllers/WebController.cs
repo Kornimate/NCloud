d95b891e53590ccde8c1a957eb370943cc02c16a
kornimate <kornimate@gmail.com>
2024-04-22 18:01:28 +0200

    web views started to elaborate

--- NCloud/NCloud/Controllers/WebController.cs
+++ NCloud/NCloud/Controllers/WebController.cs
@@ -7,14 +7,19 @@ namespace NCloud.Controllers
     [AllowAnonymous]
     public class WebController : Controller
     {
-        public async Task<IActionResult> Download (string path)
+        public async Task<IActionResult> Details(string path)
         {
             path = HashManager.DecryptString(path);
 
-            return await Task.FromResult<IActionResult>(Content(path));
+            return await Task.FromResult<IActionResult>(View());
         }
 
-        public async Task<IActionResult> Details(string path)
+        public async Task<IActionResult> Back(string path)
+        {
+            return null!;
+        }
+
+        public async Task<IActionResult> Download (string path)
         {
             path = HashManager.DecryptString(path);
 
---
+++ NCloud/NCloud/ViewModels/WebDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Castle.Core;
+using NCloud.Models;
+
+namespace NCloud.ViewModels
+{
+    public class WebDetailsViewModel
+    {
+        public List<CloudFile> Files { get; set; }
+        public List<CloudFolder> Folders { get; set; }
+        public string CurrentPathShow { get; set; }
+        public string CurrentPath { get; set; }
+        public WebDetailsViewModel(List<CloudFile> files, List<CloudFolder> folders, string currentPathShow, string currentPath)
+        {
+            Files = files;
+            Folders = folders;
+            CurrentPathShow = currentPathShow;
+            CurrentPath = currentPath;
+        }
+    }
+}
