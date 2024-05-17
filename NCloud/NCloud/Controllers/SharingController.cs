09788acda7f036b14dc7bfecbc542c135ba99205
kornimate <kornimate@gmail.com>
2024-05-18 00:58:28 +0200

    refactored sharing controller and web controller, post method introduced instead of get in web sharing controller for single file download

--- NCloud/NCloud/Controllers/SharingController.cs
+++ NCloud/NCloud/Controllers/SharingController.cs
@@ -5,6 +5,7 @@ using NCloud.ConstantData;
 using NCloud.DTOs;
 using NCloud.Models;
 using NCloud.Services;
+using NCloud.Services.Exceptions;
 using NCloud.Users;
 using NCloud.ViewModels;
 using System.IO.Compression;
@@ -28,7 +29,7 @@ namespace NCloud.Controllers
         public async Task<IActionResult> Details(string? folderName = null)
         {
             SharedPathData pathdata = await GetSessionSharedPathData();
-            
+
             try
             {
 
@@ -81,7 +82,7 @@ namespace NCloud.Controllers
         /// <returns>Redirection to details</returns>
         public async Task<IActionResult> Home()
         {
-            if(!await SetSessionSharedPathData(new SharedPathData()))
+            if (!await SetSessionSharedPathData(new SharedPathData()))
             {
                 AddNewNotification(new Error("Error while handling session for shared directory"));
             }
@@ -98,7 +99,9 @@ namespace NCloud.Controllers
         {
             if (String.IsNullOrWhiteSpace(folderName))
             {
-                return RedirectToAction("Details","Sharing");
+                AddNewNotification(new Error("Invalid folder name"));
+
+                return RedirectToAction("Details", "Sharing");
             }
 
             return await Download(new List<string>()
@@ -120,7 +123,9 @@ namespace NCloud.Controllers
         {
             if (String.IsNullOrWhiteSpace(fileName))
             {
-                return View("Error");
+                AddNewNotification(new Error("Invalid folder name"));
+
+                return RedirectToAction("Details", "Sharing");
             }
 
             return await Download(new List<string>()
@@ -132,6 +137,10 @@ namespace NCloud.Controllers
             connectedToApp: true);
         }
 
+        /// <summary>
+        /// Action method to show downloadable shared items in current sharing state
+        /// </summary>
+        /// <returns>View with downloadable items</returns>
         public async Task<IActionResult> DownloadItems()
         {
             SharedPathData pathData = await GetSessionSharedPathData();
@@ -161,6 +170,11 @@ namespace NCloud.Controllers
             }
         }
 
+        /// <summary>
+        /// Action method to handle post request to download items from current sharing state
+        /// </summary>
+        /// <param name="vm">List of items to download wrapped in DriveDownloadViewModel</param>
+        /// <returns>Download action</returns>
         [HttpPost]
         [ValidateAntiForgeryToken, ActionName("DownloadItems")]
         public async Task<IActionResult> DownloadItemsFromForm([Bind("ItemsForDownload")] DriveDownloadViewModel vm)
@@ -168,35 +182,67 @@ namespace NCloud.Controllers
             return await Download(vm.ItemsForDownload ?? new(), await service.ChangePathStructure((await GetSessionSharedPathData()).CurrentPath), RedirectToAction("Details", "Sharing"), connectedToApp: true);
         }
 
+        /// <summary>
+        /// Action method to handle folder disconnection from app
+        /// </summary>
+        /// <param name="itemName">Name of folder</param>
+        /// <returns>Json containing a bool value if action was successful and message for action success</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DisconnectDirectoryFromApp([FromBody] string itemName)
         {
-            SharedPathData pathData = await GetSessionSharedPathData();
-
-            if (await service.DisconnectDirectoryFromApp(await service.ChangePathStructure(pathData.CurrentPath), itemName, await userManager.GetUserAsync(User)))
+            try
             {
-                return Json(new ConnectionDTO { Success = true, Message = "Directory and items inside disconnected from application" });
+                SharedPathData pathData = await GetSessionSharedPathData();
+
+                if (await service.DisconnectDirectoryFromApp(await service.ChangePathStructure(pathData.CurrentPath), itemName, await userManager.GetUserAsync(User)))
+                {
+                    return Json(new ConnectionDTO { Success = true, Message = "Directory and items inside disconnected from application" });
+                }
+                else
+                {
+                    return Json(new ConnectionDTO { Success = false, Message = "Error while disconnecting directory from application" });
+                }
             }
-            else
+            catch (CloudFunctionStopException ex)
+            {
+                return Json(new ConnectionDTO { Success = false, Message = $"Error - {ex.Message}" });
+            }
+            catch (Exception)
             {
                 return Json(new ConnectionDTO { Success = false, Message = "Error while disconnecting directory from application" });
             }
         }
 
+        /// <summary>
+        /// Action method to handle file disconnection from app
+        /// </summary>
+        /// <param name="itemName">Name of file</param>
+        /// <returns>Json containing a bool value if action was successful and message for action success</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<JsonResult> DisconnectFileFromApp([FromBody] string itemName)
         {
-            SharedPathData pathData = await GetSessionSharedPathData();
+            try
+            {
+                SharedPathData pathData = await GetSessionSharedPathData();
 
-            if (await service.DisconnectFileFromApp(await service.ChangePathStructure(pathData.CurrentPath), itemName, await userManager.GetUserAsync(User)))
-            {
-                return Json(new ConnectionDTO { Success = true, Message = "File disconnected from application" });
+                if (await service.DisconnectFileFromApp(await service.ChangePathStructure(pathData.CurrentPath), itemName, await userManager.GetUserAsync(User)))
+                {
+                    return Json(new ConnectionDTO { Success = true, Message = "File disconnected from application" });
+                }
+                else
+                {
+                    return Json(new ConnectionDTO { Success = false, Message = "Error while disconnecting file from application!" });
+                }
             }
-            else
+            catch (CloudFunctionStopException ex)
             {
-                return Json(new ConnectionDTO { Success = false, Message = "Error while disconnecting file from application!" });
+                return Json(new ConnectionDTO { Success = false, Message = $"Error - {ex.Message}" });
+            }
+            catch (Exception)
+            {
+                return Json(new ConnectionDTO { Success = false, Message = "Error while disconnecting file from application" });
             }
         }
     }
