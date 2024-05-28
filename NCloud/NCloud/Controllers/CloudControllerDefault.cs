34edeaff6a955235f8d69382ece170cca0326fec
kornimate <kornimate@gmail.com>
2024-05-28 22:19:08 +0200

    bugfix for download

--- NCloud/NCloud/Controllers/CloudControllerDefault.cs
+++ NCloud/NCloud/Controllers/CloudControllerDefault.cs
@@ -238,6 +238,14 @@ namespace NCloud.Controllers
 
                                 try
                                 {
+                                    SharedFile file = await service.GetSharedFileByPathAndName(cloudPath, name);
+
+                                    if(connectedToApp && !file.ConnectedToApp)
+                                        throw new CloudFunctionStopException("File is not shared in app");
+
+                                    if (connectedToWeb && !file.ConnectedToWeb)
+                                        throw new CloudFunctionStopException("File is not shared on web");
+
                                     FileStream stream = new FileStream(Path.Combine(service.ServerPath(cloudPath), name), FileMode.Open, FileAccess.Read, FileShare.Read);
 
                                     return File(stream, MimeTypeManager.GetMimeType(name), name);
