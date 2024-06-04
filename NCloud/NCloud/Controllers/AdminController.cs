6191bc01d5c34216166002165d6d71b34ce37e2d
kornimate <kornimate@gmail.com>
2024-06-04 18:10:37 +0200

    Admin dashboard created for admins

---
+++ NCloud/NCloud/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NCloud.Controllers
+{
+    /// <summary>
+    /// Class to handle actions only possible for admin users
+    /// </summary>
+    public class AdminController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return await Task.FromResult<IActionResult>(View());
+        }
+    }
+}
